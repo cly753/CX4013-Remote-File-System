d821bfd8b505ae6184bdb85d30424f9b0fd50242
cly753 <cly753@gmail.com>
2016-02-27 21:45:29 +0800

    Delete unused import

--- src/main/java/com/neko/msg/NekoFieldOpcode.java
+++ src/main/java/com/neko/msg/NekoFieldOpcode.java
@@ -4,8 +4,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
